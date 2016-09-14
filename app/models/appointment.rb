2302f5ef35b93ab1dde92d0c5440f2df16867a72
fidelscodes <fidelscodes@gmail.com>
2016-09-14 18:45:55 -0400

    add show action and view for appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,17 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  # def appointment_datetime
+  #   binding.pry
+  #   self.appointment_datetime.strftime("%B %e, %Y at %H:%M")
+  # end
+
+  def patient_name
+    patient.name
+  end
+
+  def doctor_name
+    doctor.name
+  end
 end
