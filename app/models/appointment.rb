51a8f6b8b6a5b9c760ab8b46192bf938bee5d5b3
ozPop <ozziepopovas@gmail.com>
2016-09-15 14:49:49 -0400

    move some logic to models from views

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,16 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  def doctor_name
+    self.doctor.name
+  end
+
+  def patient_name
+    self.patient.name
+  end
+
+  def date_time
+    self.appointment_datetime.strftime("%B %e, %Y at %H:%M")
+  end
 end
